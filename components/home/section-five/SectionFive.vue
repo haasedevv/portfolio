48117aa6d6492a261b38cec2802c06413686002e
Joseph Haase <joseph.haase.dev@gmail.com>
2023-12-16 18:51:57 -0300

    Creates example list for experiences

--- components/home/section-five/SectionFive.vue
+++ components/home/section-five/SectionFive.vue
@@ -6,19 +6,47 @@
     <h2 class="section-five__title">Experiências profissionais.</h2>
 
     <div class="section-five__experiences">
-      <experience-card />
-      <div class="section-five__experience-division"></div>
-      <experience-card />
+      <div
+        v-for="(experience, index) in experiences"
+        :key="`experience-${index}`"
+        class="section-five__experience"
+      >
+        <experience-card :experience="experience" />
+        <div
+          v-if="index !== experiences.length - 1"
+          class="section-five__experience-division"
+        />
+      </div>
     </div>
   </section>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+  const experiences = ref([
+    {
+      company: "Code n'App",
+      role: "Desenvolvedor Front-end Treinee",
+      companyTime: "3 meses (O momento)",
+      description:
+        "Desenvolvedor Front-end Treinee Desenvolvedor Front-end Treinee Desenvolvedor Front-end Treinee Desenvolvedor Front-end Treinee",
+      technologies: "Vue, React"
+    },
+    {
+      company: "Code n'App",
+      role: "Estagiário em Desenvolvimento Front-end",
+      companyTime: "1 ano e 2 meses",
+      description:
+        "Desenvolvedor Front-end Treinee Desenvolvedor Front-end Treinee Desenvolvedor Front-end Treinee Desenvolvedor Front-end Treinee",
+      technologies: "Vue, React"
+    }
+  ]);
+</script>
 
 <style lang="scss" scoped>
   @import "@/assets/styles/colors/colors";
 
   .section-five__container {
+    padding: 0 2.25rem;
     .section-five__title {
       color: #403886;
       font-size: 2.25rem;
@@ -34,15 +62,21 @@
       flex-direction: column;
       align-items: center;
       max-width: 550px;
-      gap: 1rem;
       margin: 0 auto;
 
-      .section-five__experience-division {
-        height: 48px;
-        width: 8px;
-        border-radius: 20px;
-        background: $color-secondary;
-        user-select: none;
+      .section-five__experience {
+        width: 100%;
+        display: flex;
+        flex-direction: column;
+
+        .section-five__experience-division {
+          height: 48px;
+          width: 8px;
+          border-radius: 20px;
+          background: $color-secondary;
+          user-select: none;
+          margin: 1rem auto;
+        }
       }
     }
   }
