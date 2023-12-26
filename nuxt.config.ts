dff64344e345525e89cf712085ddbb900d389dc1
Joseph Haase <joseph.haase.dev@gmail.com>
2023-12-25 21:13:58 -0300

    Configurates variables of env

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,9 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   runtimeConfig: {
     public: {
-      apiBase: "/api"
+      serviceId: "",
+      templateId: "",
+      publicKey: ""
     }
   },
   svgo: {
